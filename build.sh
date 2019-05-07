550b99b8b9cc2c8f44e9fb94e67ce8e2b64e9757
fahmifan <fahmiirfan909@gmail.com>
2019-05-07 09:59:02 +0700

    uppdate script

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# create output folder
 mkdir -p output
-# build go binary
-GOOS=$OS GOARCH=$ARCH go build -o output/inventory ./cmd/inventory
-# build reactjs
+rm -rf ./output/*
+
+# build go binary into output folder
+# GOOS=$OS GOARCH=$ARCH go build -o output/inventory ./cmd/inventory
+GOOS=linux GOARCH=amd64 go build -o output/inventory-linux-amd64 ./cmd/inventory
+GOOS=windows GOARCH=amd64 go build -o output/inventory-win-amd64.exe ./cmd/inventory
+GOOS=windows GOARCH=386 go build -o output/inventory-win-386.exe ./cmd/inventory
+
+# build reactjs into output folder
 cd web && npm run build && rm -rf ../output/static && cp -r build ../output/static
